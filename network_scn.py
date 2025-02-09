1b35e43c7718567fd7b2fdfe573a6a4cd79805ce
Anirudh <aniamitgupta@gmail.com>
2025-02-09 19:05:52 +0530

    srp

--- network_scn.py
+++ network_scn.py
@@ -4,6 +4,8 @@ def scan(ip):
     arp_req = sc.ARP(pdst=ip)
     broadcast = sc.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_fin_packet = broadcast / arp_req
-    print(arp_fin_packet.summary())
+    ans, unanswered = sc.srp(arp_fin_packet, timeout=1)
+    print(ans)
+    #print(arp_fin_packet.summary())
 
 scan("10.0.2.2/24")
