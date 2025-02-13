131e44fa86af542b86e7470dfc7040fb27b5fade
Anirudh Gupta <aniamitgupta@gmail.com>
2025-02-13 20:53:55 +0530

    csv1

--- network_scn.py
+++ network_scn.py
@@ -2,6 +2,8 @@ import scapy.all as sc
 import argparse
 import ipaddress
 from mac_vendor_lookup import MacLookup
+import csv
+import datetime
 
 def validate_ip(ip_addr):
     try:
@@ -16,28 +18,29 @@ def scan(ip):
     arp_fin_packet = broadcast / arp_req
     answered = sc.srp(arp_fin_packet, timeout=1, verbose=False)[0]
 
+    MacLookup().update_vendors()
     clients_list = []
     for element in answered:
-        client_dict = {"IP":element[1].psrc, "MAC":element[1].hwsrc}
+        try:
+            vendor = MacLookup().lookup(element[1].hwsrc)
+        except Exception:
+            vendor = "Unknown"
+        client_dict = {"IP":element[1].psrc, "MAC":element[1].hwsrc, "vendor":vendor}
         clients_list.append(client_dict)
     return clients_list
 
 
 def print_info(result_list):
-    MacLookup().update_vendors()
     print("IP Address \t\t\tMAC\t\tVendors")
     print("----------------------------------------------------------")
     for client in result_list:
-        try:
-            vendor = MacLookup().lookup(client["MAC"])
-        except Exception:
-            vendor = "Unknown"
-        print(client["IP"] + "\t\t " + client["MAC"] + "\t" + vendor)
+        print(client["IP"] + "\t\t " + client["MAC"] + "\t" + client["vendor"])
 
 def arguments():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("-i","--ip-range", dest="ip_range", help="specify the IP range to scan")
+    parser.add_argument("-c","--csv", dest="csv_file", help="name for csv file")
     value = parser.parse_args()
 
     if not value.ip_range or not validate_ip(value.ip_range):
@@ -45,7 +48,18 @@ def arguments():
         exit(0)
     else:
         return value
+    
+def save_as_csv(result_list, filename):
+    with open(filename,mode="w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(["IP Address", "MAC", "Vendor", "Timestamp"])
+
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    for client in result_list:
+        writer.writerow([client["IP"], client["MAC"], client["vendor"], timestamp])
+
 
 net_range = arguments()
 scanresult = scan(net_range.ip_range)
 print_info(scanresult)
+save_as_csv(net_range.csv_file)
