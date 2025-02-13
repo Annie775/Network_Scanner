68d08c140c29f5d2b446d9aec0d22a5930fdcacb
Anirudh <aniamitgupta@gmail.com>
2025-02-13 21:37:05 +0530

    csv

--- network_scn.py
+++ network_scn.py
@@ -3,7 +3,8 @@ import argparse
 import ipaddress
 from mac_vendor_lookup import MacLookup
 import csv
-import datetime
+from datetime import datetime
+import os
 
 def validate_ip(ip_addr):
     try:
@@ -39,27 +40,45 @@ def print_info(result_list):
 def arguments():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-i","--ip-range", dest="ip_range", help="specify the IP range to scan")
-    parser.add_argument("-c","--csv", dest="csv_file", help="name for csv file")
+    parser.add_argument("-i","--ip-range", dest="ip_range", help="Specify the IP range to scan (e.g., 192.168.1.1/24)", required=True)
+    parser.add_argument("-c","--csv", dest="csv_file", help="Name for the CSV file (default: default_scan_results.csv)")
     value = parser.parse_args()
 
     if not value.ip_range or not validate_ip(value.ip_range):
         print("[-] Please Specify a valid IP range with -i")
         exit(0)
     else:
+        if not value.csv_file:
+            value.csv_file = "default_scan_results.csv"
         return value
-    
-def save_as_csv(result_list, filename):
-    with open(filename,mode="w", newline="") as file:
-        writer = csv.writer(file)
-        writer.writerow(["IP Address", "MAC", "Vendor", "Timestamp"])
 
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    for client in result_list:
-        writer.writerow([client["IP"], client["MAC"], client["vendor"], timestamp])
+
+def save_as_csv(result_list, filename):
+    if not result_list:
+        print("[-] No devices found. Nothing to save.")
+        return
+
+    file_exists = os.path.isfile(filename)
+    try:
+        with open(filename, mode="a", newline="") as file:
+            writer = csv.writer(file)
+
+            if not file_exists:
+                writer.writerow(["IP Address", "MAC", "Vendor", "Timestamp"])
+
+            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            for client in result_list:
+                writer.writerow([client["IP"], client["MAC"], client["vendor"], timestamp])
+
+        print(f"\n[+] Appended scan results to {filename}")
+
+    except Exception as e:
+        print(f"[-] Error appending to the file: {e}")
 
 
 net_range = arguments()
 scanresult = scan(net_range.ip_range)
 print_info(scanresult)
-save_as_csv(net_range.csv_file)
+
+if net_range.csv_file:
+    save_as_csv(scanresult,net_range.csv_file)
